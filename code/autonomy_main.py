969a35f928dd5141788191933c8123737503b35e
Andrew Jones <aajones@iastate.edu>
2017-03-01 16:25:58 -0600

    continue autonomy

--- code/autonomy_main.py
+++ code/autonomy_main.py
@@ -1,8 +1,19 @@
 # Sailbot main autonomy program
 
 
+#	mode determines the main function of Sailbot
+#	Mode 1: search for bouy 
+#	Mode 2: go towards bouy
+#	Mode 3: send finish signal and exit course
+mode = 1
+
 def Main():
-	
+	if mode == 1:
+
+	else if mode == 2:
+
+	else if mode == 3:
+
 
 #Protects main from being run when imported and only run when executed.
 #Runs main()
